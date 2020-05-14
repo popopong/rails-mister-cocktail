0a3a70e09964251e346bef39acf65e949df1cc6f
Poyan Ng <poyanng.npy@gmail.com>
2020-05-14 16:16:40 -0400

    simple form is smarter than me

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Cleaning database..."
+Ingredient.destroy_all
+
+open_url_ing = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredients = JSON.parse(open_url_ing)
+
+ingredients["drinks"].each do |ing|
+  ingredient = ing["strIngredient1"]
+  Ingredient.create(name: ingredient)
+end
+
+# open_url_drinks = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+# drinks = JSON.parse(open_url_drinks)
+
+# drinks["drinks"].each do |details|
+  
+# end
+
+puts "Created ingredient and cocktail seeds."
